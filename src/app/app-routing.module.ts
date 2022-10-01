dcecae1ca882713a0e9eb1f7ca803715d19cab88
andreschacon <johnstartreck@gmail.com>
2022-10-01 17:33:33 -0500

    actualizando los estilos y agregando nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+// Components
+import { LoginComponent } from './components/login/login.component';
+import { PrivateTasksComponent } from './components/private-tasks/private-tasks.component';
+import { RegisterComponent } from './components/register/register.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 
 const routes: Routes = [
-  {path:'login',component: LoginComponent},
   {path:'*',pathMatch: 'full', redirectTo: 'login'},
-  {path:'register',component: RegisterComponent}
+  {path:'login',component: LoginComponent},
+  {path:'register',component: RegisterComponent},
+  {path: 'task', component: TasksComponent},
+  {path: 'private-task', component: PrivateTasksComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { PrivateTasksComponent } from './components/private-tasks/private-tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    TasksComponent,
+    PrivateTasksComponent
   ],
   imports: [
     BrowserModule,
